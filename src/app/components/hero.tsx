e7b9cc5ae60bcf0e1ee4f70993a50568193de268
Francisco Salazar (Franvierdev) <xxaverii@gmail.com>
2024-07-05 19:58:59 -0400

    asd

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -8,16 +8,16 @@ export default function Hero({ fraunces }: any) {
         width={2880 / 2}
         height={1600 / 2}
         alt="reference"
-        className="max-sm:hidden"
+        className="max-lg:hidden"
       />
       <Image
         src="/mobile/image-header.jpg"
         width={750 / 2}
         height={1076 / 2}
         alt="reference"
-        className="sm:hidden"
+        className="lg:hidden h-screen"
       />
-      <div className=" top-36 lg:top-52 lg:w-[1440px] absolute font-extrabold text-white  max-lg:text-4xl lg:text-6xl tracking-[0.2em]  ">
+      <div className=" top-36 lg:top-52 w-[375px] lg:w-[1440px] absolute font-extrabold text-white  max-lg:text-4xl lg:text-6xl tracking-[0.2em]  ">
         <h1 className={fraunces.className}>WE ARE CREATIVES</h1>
         <Image
           src="/icon-arrow-down.svg"
